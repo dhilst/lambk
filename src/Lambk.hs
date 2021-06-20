bf2fe0decd5ae595ed6c70320ea1300eb8bac644
Daniel Hilst <daniel@versatushpc.com.br>
2021-06-20 00:31:40 -0300

    Testing passing

--- src/Lambk.hs
+++ src/Lambk.hs
@@ -1,54 +1,49 @@
-{-# LANGUAGE QuasiQuotes #-}
 module Lambk where
 
-import           Control.Monad.Reader
-import qualified Data.Map.Strict         as M
-import           Data.String.Interpolate (i)
-import           Debug.Trace
-import           Test.Hspec
-import           Text.Show.Unicode
+import Control.Monad.Reader
+import qualified Data.Map.Strict as M
+import Debug.Trace
+import Test.Hspec
+import Text.Show.Unicode
+
 {-# ANN module ("hlint: ignore Use <$>") #-}
-
-
 
 data Term
   = Var String
-  | FVar String
   | Lamb String Term
   | App Term Term
   deriving (Eq)
 
-
 instance Show Term where
-  show (Var x)     = x
-  show (Lamb x t)  = "(λ" ++ x  ++ "." ++ show t ++ ")"
-  show (App t1 t2) = [i|(#{show t1} #{show t2})|]
-  show (FVar x)    = x
-
+  show (Var x) = x
+  show (Lamb x t) = "(λ" ++ x ++ "." ++ show t ++ ")"
+  show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
 
 isVal :: Term -> Bool
-isVal (Lamb _ _) =  True
-isVal (FVar _) = True
-isVal (Var _) = False
-isVal (App t1 _) = case t1 of
-  Lamb _ _ -> False
-  _        -> True
-
-
-
-type Env = M.Map String Term
+isVal (Lamb _ _) = True
+isVal (Var _) = True
+isVal (App t1 _) =
+  case t1 of
+    Lamb _ _ -> False
+    _ -> True
 
 eval :: Term -> Term
-eval t = eval' t M.empty
-  where eval' :: Term -> Env -> Term
-        eval' t env
-          | isVal t = t
-          | otherwise = case t of
-              Var v -> case M.lookup v env of
-                Just t  -> t
-                Nothing -> FVar v
-              App t1 t2 ->
-                case t1 of
-                  Lamb x body -> let v = eval' t2 env
-                                 in eval' v $ M.insert x v env
-                  _           -> t
+eval t
+  | isVal t = t
+  | otherwise =
+    case t of
+      App t1 t2 ->
+        case t1 of
+          Lamb x body ->
+            let v2 = eval t2
+             in subs x v2 body
+  where
+    subs :: String -> Term -> Term -> Term
+    subs x replace expr =
+      case expr of
+        Var y
+          | x == y -> replace
+        (Lamb v body)
+          | v /= x -> Lamb v (subs x replace body)
+        (App t1 t2) -> App (subs x replace t1) (subs x replace t2)
+        _ -> expr
--- test/Main.hs
+++ test/Main.hs
@@ -1,32 +1,36 @@
 module Main where
 
-import           Lambk
-import           Parser
-import           Test.Hspec
-
+import Lambk
+import Parser
+import Test.Hspec
 
 parserTest :: IO ()
-parserTest = hspec $do
-  describe "Parses" $ do
-    it "Should parse char" $ do
-      let parseC = parseChar 'c'
-        in runParser parseC "cx" `shouldBe` Just ("x", 'c')
-    it "Should parse lambda term" $ do
-      let result = show . parse $ "(λx.x)"
-        in result `shouldBe` "(λx.x)"
-    it "Should parse var term" $ do
-      let result = show . parse $ "x"
-        in result `shouldBe` "x"
-    it "Should parse app term" $ do
-      let result = show . parse $ "((λx.x) y)"
-        in result `shouldBe` "((λx.x) y)"
+parserTest =
+  hspec $ do
+    describe "Parses" $ do
+      it "Should parse char" $ do
+        let parseC = parseChar 'c'
+         in runParser parseC "cx" `shouldBe` Just ("x", 'c')
+      it "Should parse lambda term" $ do
+        let result = show . parse $ "(λx.x)"
+         in result `shouldBe` "(λx.x)"
+      it "Should parse var term" $ do
+        let result = show . parse $ "x"
+         in result `shouldBe` "x"
+      it "Should parse app term" $ do
+        let result = show . parse $ "((λx.x) y)"
+         in result `shouldBe` "((λx.x) y)"
 
 evalTest :: IO ()
-evalTest = hspec $ do
-  describe "Beta reduction" $ do
-    it "reduces to y" $ do
-      show (eval (App (Lamb "x" (Var "x")) (Var "y"))) `shouldBe` "y"
-
+evalTest =
+  hspec $ do
+    describe "Beta reduction" $ do
+      it "((λx.(x x)) y) => (y y)" $ do
+        let result = show . eval . parse $ "((λx.(x x)) y)"
+         in result `shouldBe` "(y y)"
+      it "((λx.(x z)) y) => (y z)" $ do
+        let result = show . eval . parse $ "((λx.(x z)) y)"
+         in result `shouldBe` "(y z)"
 
 main :: IO ()
 main = do
